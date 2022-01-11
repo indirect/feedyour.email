f5e42db01d2b87cd1064f4424c4eef950f54f943
Andre Arko <andre@arko.net>
2022-01-11 01:03:20 -0800

    move the meta tags into a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def sharing_meta_tags(title: "Feed Your Email")
+    description = "Generate an email address you can use for any newsletter, and a corresponding feed you can use to read those emails."
+    image = root_url + "card.jpg"
+
+    [
+      # OpenGraph
+      tag.meta(property: "og:title", content: title),
+      tag.meta(property: "og:description", content: description),
+      tag.meta(property: "og:url", content: root_url),
+      tag.meta(property: "og:image", content: image),
+      tag.meta(property: "og:site_name", content: title),
+      tag.meta(property: "og:locale", content: "en_US"),
+      tag.meta(property: "og:type", content: "website"),
+      # Twitter
+      tag.meta(property: "twitter:card",  content: "summary_large_image"),
+      tag.meta(property: "twitter:title", content: title),
+      tag.meta(property: "twitter:description", content: description),
+      tag.meta(property: "twitter:image", content: image),
+      tag.meta(property: "twitter:site", content: root_url),
+      tag.meta(property: "twitter:creator", content: "@indirect"),
+      # Icons
+      tag.link(rel: "icon", type: "image/svg+xml", href: "/favicon.svg"),
+      tag.link(rel: "alternate icon", href: "/favicon.ico"),
+    ].join("\n").html_safe
+  end
 end
