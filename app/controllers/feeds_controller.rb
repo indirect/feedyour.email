fbb1629eaf2e3e1fca4853f6da614f5eee84af88
Andre Arko <andre@arko.net>
2022-01-11 00:27:01 -0800

    disable feed list/update/destroy

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,22 +1,8 @@
 class FeedsController < ApplicationController
   before_action :set_feed, only: %i[ show edit update destroy ]
 
-  # GET /feeds or /feeds.json
-  def index
-    @feeds = Feed.all
-  end
-
   # GET /feeds/1 or /feeds/1.json
   def show
-  end
-
-  # GET /feeds/new
-  def new
-    @feed = Feed.new
-  end
-
-  # GET /feeds/1/edit
-  def edit
   end
 
   # POST /feeds or /feeds.json
@@ -34,37 +20,53 @@ class FeedsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /feeds/1 or /feeds/1.json
-  def update
-    respond_to do |format|
-      if @feed.update(feed_params)
-        format.html { redirect_to feed_url(@feed), notice: "Feed was successfully updated." }
-        format.json { render :show, status: :ok, location: @feed }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
+  if Rails.env.development?
+    # GET /feeds or /feeds.json
+    def index
+      @feeds = Feed.all
+    end
+
+    # GET /feeds/new
+    def new
+      @feed = Feed.new
+    end
+
+    # GET /feeds/1/edit
+    def edit
+    end
+
+    # PATCH/PUT /feeds/1 or /feeds/1.json
+    def update
+      respond_to do |format|
+        if @feed.update(feed_params)
+          format.html { redirect_to feed_url(@feed), notice: "Feed was successfully updated." }
+          format.json { render :show, status: :ok, location: @feed }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @feed.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    # DELETE /feeds/1 or /feeds/1.json
+    def destroy
+      @feed.destroy
+
+      respond_to do |format|
+        format.html { redirect_to feeds_url, notice: "Feed was successfully destroyed." }
+        format.json { head :no_content }
       end
     end
   end
 
-  # DELETE /feeds/1 or /feeds/1.json
-  def destroy
-    @feed.destroy
-
-    respond_to do |format|
-      format.html { redirect_to feeds_url, notice: "Feed was successfully destroyed." }
-      format.json { head :no_content }
-    end
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_feed
+    @feed = Feed.where(token: params[:id]).first
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_feed
-      @feed = Feed.where(token: params[:id]).first
-    end
-
-    # Only allow a list of trusted parameters through.
-    def feed_params
-      params.fetch(:feed, {}).permit(:name)
-    end
+  # Only allow a list of trusted parameters through.
+  def feed_params
+    params.fetch(:feed, {}).permit(:name)
+  end
 end
