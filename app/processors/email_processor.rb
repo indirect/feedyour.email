2b2309fc3b58b95878a85d32661aa57df8acd9c9
Andre Arko <andre@arko.net>
2022-01-12 14:15:48 -0800

    simplify email processing

--- app/processors/email_processor.rb
+++ app/processors/email_processor.rb
@@ -1,20 +1,23 @@
 class EmailProcessor
+
   def initialize(email)
     @email = email
   end
 
   def process(token: nil)
-    return unless feed_token
+    return unless feed
 
-    Post.create!(
-      feed: Feed.where(token: feed_token).first || (Rails.env.development? && Feed.order(:updated_at).last),
-      payload: @email.to_h,
-      token: token
-    )
+    Post.create!(feed: feed, payload: @email.to_h, token: token)
+  end
+
+  def feed
+    @feed ||= Feed.where(token: feed_token).first
+    @feed ||= Feed.order(:updated_at).last if Rails.env.development?
   end
 
   def feed_token
     contact = @email.to.find{|t| t[:host] == "feedyour.email" }
     contact && contact[:token]
   end
+
 end
