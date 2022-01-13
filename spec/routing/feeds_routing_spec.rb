767f93d3415b3cf9255b24a62622270389abbe6f
Andre Arko <andre@arko.net>
2022-01-12 23:04:56 -0800

    update route specs

--- spec/routing/feeds_routing_spec.rb
+++ spec/routing/feeds_routing_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 RSpec.describe FeedsController, type: :routing do
   describe "routing" do
-    it "routes to #index" do
-      expect(get: "/feeds").to route_to("feeds#index")
+    it "routes the root to feeds#new" do
+      expect(get: "/").to route_to("feeds#new")
     end
 
     it "routes to #new" do
@@ -14,25 +14,8 @@ RSpec.describe FeedsController, type: :routing do
       expect(get: "/feeds/1").to route_to("feeds#show", id: "1")
     end
 
-    it "routes to #edit" do
-      expect(get: "/feeds/1/edit").to route_to("feeds#edit", id: "1")
-    end
-
-
     it "routes to #create" do
       expect(post: "/feeds").to route_to("feeds#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(put: "/feeds/1").to route_to("feeds#update", id: "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(patch: "/feeds/1").to route_to("feeds#update", id: "1")
-    end
-
-    it "routes to #destroy" do
-      expect(delete: "/feeds/1").to route_to("feeds#destroy", id: "1")
     end
   end
 end
