dfd9e77d8bfc07d7a520efb1d2e3c7e16cc913bd
Andre Arko <andre@arko.net>
2022-01-13 00:02:55 -0800

    post spec

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,14 +1,24 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "posts/index", type: :view do
-  before(:each) do
-    assign(:posts, [
-      Post.create!(),
-      Post.create!()
-    ])
+  before do
+    @feed = Feed.create!(token: "abc123", name: "somename")
+    @post = @feed.posts.create!(token: "ptoken", payload: {
+      subject: "post title",
+      raw_html: "<h1>post!</h1>",
+      from: {full: "Adora <adora@princess.alliance>"}
+    })
+    assign :feed, @feed
+    assign :posts, [@post]
   end
 
   it "renders a list of posts" do
     render
+
+    assert_select "a[href='/posts/ptoken']" do
+      assert_select "p", "post title"
+      assert_select "p", "Adora <adora@princess.alliance>"
+      assert_select "time", /#{@post.updated_at.to_date.to_formatted_s(:short)}/
+    end
   end
 end
