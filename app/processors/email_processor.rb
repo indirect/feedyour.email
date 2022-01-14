dafb440634e7145b55e4cd190cc6f2d7c7682443
Andre Arko <andre@arko.net>
2022-01-13 19:51:13 -0800

    add EmailProcessor payload helper

--- app/processors/email_processor.rb
+++ app/processors/email_processor.rb
@@ -1,12 +1,25 @@
 class EmailProcessor
   cattr_accessor :default_to_newest_feed, default: false
+  attr_writer :feed
 
-  def initialize(email)
+  def self.for_payload(payload, feed: nil)
+    json = JSON.parse(payload)
+    params = Griddler::Postmark::Adapter.normalize_params(json.deep_symbolize_keys)
+    email = Griddler::Email.new(params)
+    new(email, feed: feed)
+  end
+
+  def initialize(email, feed: nil)
     @email = email
+    @feed = feed
   end
 
   def process(token: nil)
-    Post.create!(feed: Feed.find_by(token: feed_token), payload: @email.to_h, token: token)
+    Post.create!(feed: feed, payload: @email.to_h, token: token)
+  end
+
+  def feed
+    @feed ||= Feed.find_by(token: feed_token)
   end
 
   def feed_token
