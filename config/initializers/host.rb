7d4f87716b8e230aa339e0153e94cf33b90d810f
Andre Arko <andre@arko.net>
2022-01-18 03:26:18 -0800

    jam HOST into all the url things
    christ why are there so many this is clearly something that should be set in one central place good god

--- config/initializers/host.rb
+++ config/initializers/host.rb
@@ -1,3 +1,14 @@
 if ENV.key?("HOST")
   Rails.application.config.hosts << ENV["HOST"]
+
+  Rails.application.config.after_initialize do
+    ApplicationController.renderer.defaults.merge!(http_host: ENV["HOST"], https: true)
+
+    [
+      Rails.application.default_url_options,
+      Rails.application.routes.default_url_options,
+      ApplicationController.default_url_options,
+      ApplicationMailer.default_url_options
+    ].each { |opts| opts[:host] = ENV["HOST"] }
+  end
 end
