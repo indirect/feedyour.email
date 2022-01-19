0b3e7e20e4da2f1deee22690a21004cc2c5e9900
Andre Arko <andre@arko.net>
2022-01-19 00:11:15 -0800

    configure TLD length for review apps

--- config/initializers/host.rb
+++ config/initializers/host.rb
@@ -3,6 +3,11 @@ host = ENV.key?("HEROKU_APP_NAME") ? "#{ENV["HEROKU_APP_NAME"]}.herokuapp.com" :
 if host
   Rails.application.config.hosts << host << /.+?\.#{host}/
 
+  if ENV.key?("HEROKU_APP_NAME")
+    # treat herokuapp.com like co.uk, dropping both
+    ActionDispatch::Http::URL.tld_length = 2
+  end
+
   Rails.application.config.after_initialize do
     ApplicationController.renderer.defaults.merge!(http_host: host, https: true)
 
