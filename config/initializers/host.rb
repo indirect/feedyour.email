a8eb51eedc79e547d4339a9463276de33a4e5b92
Andre Arko <andre@arko.net>
2022-01-19 00:05:13 -0800

    allow HOST from HEROKU_APP_NAME as well

--- config/initializers/host.rb
+++ config/initializers/host.rb
@@ -1,8 +1,10 @@
-if ENV.key?("HOST")
-  Rails.application.config.hosts << ENV["HOST"] << /.+?\.#{ENV["HOST"]}/
+host = ENV.key?("HEROKU_APP_NAME") ? "#{ENV["HEROKU_APP_NAME"]}.herokuapp.com" : ENV["HOST"]
+
+if host
+  Rails.application.config.hosts << host << /.+?\.#{host}/
 
   Rails.application.config.after_initialize do
-    ApplicationController.renderer.defaults.merge!(http_host: ENV["HOST"], https: true)
+    ApplicationController.renderer.defaults.merge!(http_host: host, https: true)
 
     # This super gross hack is to work around a bug in Rails that doesn't apply the defaults
     # to existing instances of Renderer. https://github.com/rails/rails/issues/41795
@@ -13,6 +15,6 @@ if ENV.key?("HOST")
       Rails.application.routes.default_url_options,
       ApplicationController.default_url_options,
       ApplicationMailer.default_url_options
-    ].each { |opts| opts[:host] = ENV["HOST"] }
+    ].each { |opts| opts[:host] = host }
   end
 end
