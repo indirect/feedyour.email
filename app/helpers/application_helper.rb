0db3ac643987ddc1fade2eb74d505ae04e0b7b87
Andre Arko <andre@arko.net>
2022-01-22 21:09:28 -0800

    also remove opengraph images from pages#index
    this might mean that we'll get the favicon instead

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
   def sharing_meta_tags(title: "Feed Your Email")
+    if content_for?(:page_favicon)
+      return tag.link(rel: "icon", href: content_for(:page_favicon))
+    end
+
     description = "Generate an email address you can use for any newsletter, and a corresponding feed you can use to read those emails."
     image = root_url + "card.jpg"
 
-    links = [
+    [
       # OpenGraph
       tag.meta(property: "og:title", content: title),
       tag.meta(property: "og:description", content: description),
@@ -18,18 +22,11 @@ module ApplicationHelper
       tag.meta(property: "twitter:description", content: description),
       tag.meta(property: "twitter:image", content: image),
       tag.meta(property: "twitter:site", content: root_url),
-      tag.meta(property: "twitter:creator", content: "@indirect")
-    ]
+      tag.meta(property: "twitter:creator", content: "@indirect"),
 
-    # Icons
-    if content_for?(:page_favicon)
-      links << tag.link(rel: "icon", href: content_for(:page_favicon))
-    else
-      links << tag.link(rel: "icon", type: "image/svg+xml", href: "/favicon.svg")
-      links << tag.link(rel: "alternate icon", href: "/favicon.ico")
-    end
-
-    links.join("\n").html_safe # rubocop:disable Rails/OutputSafety
+      # Icons
+      tag.link(rel: "icon", type: "image/svg+xml", href: "/favicon.svg")
+    ].join("\n").html_safe # rubocop:disable Rails/OutputSafety
   end
 
   def gravatar_url(email)
