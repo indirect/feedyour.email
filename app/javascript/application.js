c8bcdab4f0711c3fe0460dbe77aa432e54c88873
Andre Arko <andre@arko.net>
2022-01-27 00:36:02 -0800

    make clipboard work after forms and back button

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,25 +1,30 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 
-document.querySelectorAll(".clipboard-copy").forEach(el => {
-  el.addEventListener("click", function(e) {
-    let code = this.querySelector("code")
-    let selection = window.getSelection()
-    selection.removeAllRanges()
-    let range = document.createRange()
-    range.selectNode(code)
-    selection.addRange(range)
-    if (navigator.clipboard) {
-      navigator.clipboard.writeText(code.innerText).then(() => {
-        this.querySelector("svg.clip-success").classList.remove("hidden")
-      }).catch(() => {
-        this.querySelector("svg.clip-failure").classList.remove("hidden")
-      }).finally(() => {
+function setUpClipboardCopy() {
+  document.querySelectorAll(".clipboard-copy").forEach(el => {
+    el.addEventListener("click", function(e) {
+      let code = this.querySelector("code")
+      let selection = window.getSelection()
+      selection.removeAllRanges()
+      let range = document.createRange()
+      range.selectNode(code)
+      selection.addRange(range)
+      if (navigator.clipboard) {
+        navigator.clipboard.writeText(code.innerText).then(() => {
+          this.querySelector("svg.clip-success").classList.remove("hidden")
+        }).catch(() => {
+          this.querySelector("svg.clip-failure").classList.remove("hidden")
+        }).finally(() => {
+          this.querySelector("svg.clip-empty").classList.add("hidden")
+        })
+      } else {
         this.querySelector("svg.clip-empty").classList.add("hidden")
-      })
-    } else {
-      this.querySelector("svg.clip-empty").classList.add("hidden")
-      this.querySelector("svg.clip-failure").classList.remove("hidden")
-    }
+        this.querySelector("svg.clip-failure").classList.remove("hidden")
+      }
+    })
   })
-})
+}
+
+document.addEventListener("turbo:load", setUpClipboardCopy)
+setUpClipboardCopy()
