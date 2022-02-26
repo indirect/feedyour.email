0e8b1bd60f4c2ad2708513358bb73b7471ba20f9
Andre Arko <andre@arko.net>
2022-02-25 23:33:37 -0800

    add ActionMailbox email processor

---
+++ app/mailboxes/post_mailbox.rb
@@ -0,0 +1,40 @@
+class PostMailbox < ApplicationMailbox
+  include ActionView::RecordIdentifier
+
+  def process(token: nil)
+    return if postmark_test? || feed_token.nil?
+
+    post = Post.create!(
+      feed: feed,
+      token: token,
+      from: mail.from_address,
+      subject: mail.subject,
+      html_body: mail.html_part.decoded,
+      text_body: mail.text_part.decoded
+    )
+
+    post.broadcast_prepend_to(feed, :posts)
+    feed.broadcast_replace_to(
+      feed,
+      target: dom_id(feed, "count"),
+      partial: "feeds/feed_count",
+      locals: {feed: feed}
+    )
+  end
+
+  def feed
+    Feed.find_by!(token: feed_token)
+  end
+
+  def feed_token
+    mail.to_addresses.find { |a| a.domain == "feedyour.email" }&.local ||
+      raise("Unknown address #{mail.to_addresses.inspect}")
+  end
+
+  private
+
+  def postmark_test?
+    mail.from.include?("support@postmarkapp.com") &&
+      mail.to.include?("mailbox+SampleHash@inbound.postmarkapp.com")
+  end
+end
