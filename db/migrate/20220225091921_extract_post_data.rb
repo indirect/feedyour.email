4616b347b1acbb104a233ea4500841cc5e3aeeae
Andre Arko <andre@arko.net>
2022-02-26 14:22:45 -0800

    oops didn't parse emails while migrating

--- db/migrate/20220225091921_extract_post_data.rb
+++ db/migrate/20220225091921_extract_post_data.rb
@@ -3,8 +3,9 @@ class ExtractPostData < ActiveRecord::Migration[7.0]
 
   def change
     Post.where.not(payload: nil).find_each do |post|
+      from = post.payload.dig("from", "full")
       post.update!(
-        from: post.payload.dig("from", "full"),
+        from: Mail::Address.wrap(from),
         subject: post.payload["subject"],
         html_body: post.payload["raw_html"],
         text_body: post.payload["raw_text"]
