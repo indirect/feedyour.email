d86c74d3ecf183044d3de1ac6f70d3211ddb7aa7
Scott Perry <dev@numist.net>
2022-03-11 22:00:16 -0800

    Make atom schema code consistent with how we're already doing json schema

--- spec/support/validate_feed.rb
+++ spec/support/validate_feed.rb
@@ -3,14 +3,13 @@ require "xml/libxml"
 
 module ValidateFeed
   JSON_SCHEMA = Rails.root.join("spec/support/json_feed_schema.json").read
+  ATOM_SCHEMA = XML::RelaxNG.document(XML::Document.file("spec/support/atom.rng.xml"))
 
   def validate_json_feed
     JSON::Validator.fully_validate(JSON_SCHEMA, response.body)
   end
 
   def validate_atom_feed(content = @response.body)
-    relaxng_document = XML::Document.file("spec/support/atom.rng.xml")
-    relaxng_schema = XML::RelaxNG.document(relaxng_document)
-    XML::Document.string(content).validate_relaxng(relaxng_schema)
+    XML::Document.string(content).validate_relaxng(ATOM_SCHEMA)
   end
 end
