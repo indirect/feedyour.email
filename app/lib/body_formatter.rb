e432d9c8e7df368cd2dbbb032b77a7d59e6585b7
Andre Arko <andre@arko.net>
2022-08-22 22:07:05 -0700

    hide the ads

--- app/lib/body_formatter.rb
+++ app/lib/body_formatter.rb
@@ -10,7 +10,7 @@ class BodyFormatter
     when "Matt Levine"
       @html_body
         .gsub("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\r\n", "")
-        .gsub("img {\nmax-width: 550px;\n}", "img { max-width: 100% }")
+        .gsub("img {\nmax-width: 550px;\n}", "[class^=\"liveintent\"] { display: none !important; }")
         .gsub("max-width: 550px; width: 100% !important", "width: 100% !important")
         .gsub("</head>", ApplicationController.render(partial: "posts/injected_header"))
         .gsub(/<td class="footer-wrapper.*/, "</tr></tbody></table></body></html>")
