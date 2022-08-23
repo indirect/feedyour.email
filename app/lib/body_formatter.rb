2e1fcef2763021f71920481735ab89d97a133067
Andre Arko <andre@arko.net>
2022-08-22 23:32:02 -0700

    make the column centered again

--- app/lib/body_formatter.rb
+++ app/lib/body_formatter.rb
@@ -10,8 +10,7 @@ class BodyFormatter
     when "Matt Levine"
       @html_body
         .gsub("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\r\n", "")
-        .gsub("img {\nmax-width: 550px;\n}", "[class^=\"liveintent\"] { display: none !important; }")
-        .gsub("max-width: 550px; width: 100% !important", "width: 100% !important")
+        .gsub("</style>", "[class^=\"liveintent\"] { display: none !important; }</style>")
         .gsub("</head>", ApplicationController.render(partial: "posts/injected_header"))
         .gsub(/<table class="footer".*/m, "</body></html>")
         .gsub(/(.{4})(\[\d+\])/) do
