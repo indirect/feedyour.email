7d4231aec56ba23d5b0a4ec0828ed259a1b2e95a
Andre Arko <andre@arko.net>
2024-01-05 20:56:12 -0800

    fix encoding when decompressing brotli strings

--- app/serializers/brotli_serializer.rb
+++ app/serializers/brotli_serializer.rb
@@ -6,6 +6,6 @@ class BrotliSerializer
   end
 
   def self.load(data)
-    data.present? ? Brotli.inflate(data) : nil
+    data.present? ? Brotli.inflate(data).force_encoding("UTF-8") : nil
   end
 end
