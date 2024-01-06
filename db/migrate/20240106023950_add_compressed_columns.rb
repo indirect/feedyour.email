eb03f1a921ce591c1f2501aa4b6abf1aac8abce5
Andre Arko <andre@arko.net>
2024-01-05 20:58:42 -0800

    fix linter violations

--- db/migrate/20240106023950_add_compressed_columns.rb
+++ db/migrate/20240106023950_add_compressed_columns.rb
@@ -1,6 +1,6 @@
 class AddCompressedColumns < ActiveRecord::Migration[7.1]
   def change
-    change_table :posts do |t|
+    change_table :posts, bulk: true do |t|
       t.binary :compressed_html_body
       t.binary :compressed_text_body
     end
