09c1b7cf3103e0b2dabf5f92407ddfec51d3b46a
Andre Arko <andre@arko.net>
2024-01-07 22:11:46 -0800

    turns out present? does a UTF-8 regex match, oops
    brotli data of course is not actually valid utf-8, on account of it
    being all bitwise compressed and stuff. whoops.

--- app/serializers/brotli_serializer.rb
+++ app/serializers/brotli_serializer.rb
@@ -2,10 +2,10 @@ require "brotli"
 
 class BrotliSerializer
   def self.dump(data)
-    data.present? ? Brotli.deflate(data) : nil
+    data ? Brotli.deflate(data) : nil
   end
 
   def self.load(data)
-    data.present? ? Brotli.inflate(data).force_encoding("UTF-8") : nil
+    data ? Brotli.inflate(data).force_encoding("UTF-8") : nil
   end
 end
