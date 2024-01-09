9995be1f61c9739bc16b98591121769a0afc59e0
Andre Arko <andre@arko.net>
2024-01-09 00:17:20 -0800

    use the litestack env dir, there's multiple files

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
+ENV["LITESTACK_DATA_PATH"] ||= "./storage"
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
