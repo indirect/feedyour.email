be287d8d0ff44f78ee8e8836fd6ae3ca443a74f2
Scott Perry <dev@numist.net>
2024-07-06 11:49:22 -0700

    Heed the rubocop

--- config/initializers/configure_sqlite3.rb
+++ config/initializers/configure_sqlite3.rb
@@ -1,12 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-if ::ActiveRecord::Base.connection_config[:adapter] == 'sqlite3'
-  if c = ::ActiveRecord::Base.connection
+if ::ActiveRecord::Base.connection_config[:adapter] == "sqlite3"
+  if (c = ::ActiveRecord::Base.connection)
     # Database configuration.
     # Pragmas that are only practical to customize at database-creation time (like `page_size`) should be avoided here.
-    c.execute 'PRAGMA main.journal_mode=WAL;'
+    c.execute "PRAGMA main.journal_mode=WAL;"
 
     # Connection configuration
-    c.execute 'PRAGMA main.synchronous=NORMAL;'
+    c.execute "PRAGMA main.synchronous=NORMAL;"
   end
 end
