8c6b5c8c3aadc4a7c5c583bf4d838688274638c2
Scott Perry <dev@numist.net>
2024-07-06 14:46:54 -0700

    Remove a warning about database configuration

--- config/initializers/configure_sqlite3.rb
+++ config/initializers/configure_sqlite3.rb
@@ -3,7 +3,6 @@
 if ::ActiveRecord::Base.connection.is_a?(ActiveRecord::ConnectionAdapters::SQLite3Adapter)
   if (c = ::ActiveRecord::Base.connection)
     # Database configuration.
-    # Pragmas that are only practical to customize at database-creation time (like `page_size`) should be avoided here.
     c.execute "PRAGMA main.journal_mode=WAL;"
 
     # Connection configuration
