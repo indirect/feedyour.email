67c36ad7dec8e4adef9d3d9ce2a9c7f8f3f9cbda
Andre Arko <andre@arko.net>
2024-08-31 14:00:14 +0800

    don't un-expire feed if fetched after expiring

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -5,7 +5,7 @@ class FeedsController < ApplicationController
 
   def show
     @feed = Feed.find_by!(token: params[:id])
-    @feed.touch(:fetched_at) unless request.format.html?
+    @feed.touch(:fetched_at) unless @feed.expired? || request.format.html?
   end
 
   def create
