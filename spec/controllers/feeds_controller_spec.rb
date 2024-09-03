6f686b4225151b973d00d4ddf1ad34f681b7a7ef
Andre Arko <andre@arko.net>
2024-09-03 13:43:47 +0800

    tiny feeds controller spec to narrow down issues

---
+++ spec/controllers/feeds_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe FeedsController, type: :controller do
+  describe "#new" do
+    it "succeeds" do
+      get :new
+      expect(response).to be_successful
+    end
+  end
+
+  describe "#create" do
+    it "suceeds and redirects" do
+      post :create, params: {name: "Some name"}
+      expect(response).to be_redirect
+    end
+  end
+
+  describe "#show" do
+    it "succeeds" do
+      get :show, params: {id: "abc123"}
+      expect(response).to be_successful
+    end
+  end
+end
