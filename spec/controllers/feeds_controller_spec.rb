65f13710af21ba5999141b115103ce7160886657
Andre Arko <andre@arko.net>
2024-09-03 13:45:13 +0800

    test atom and json

--- spec/controllers/feeds_controller_spec.rb
+++ spec/controllers/feeds_controller_spec.rb
@@ -20,5 +20,15 @@ RSpec.describe FeedsController, type: :controller do
       get :show, params: {id: "abc123"}
       expect(response).to be_successful
     end
+
+    it "succeeds with json" do
+      get :show, params: {id: "abc123"}, format: :json
+      expect(response).to be_successful
+    end
+
+    it "succeeds with atom" do
+      get :show, params: {id: "abc123"}, format: :atom
+      expect(response).to be_successful
+    end
   end
 end
