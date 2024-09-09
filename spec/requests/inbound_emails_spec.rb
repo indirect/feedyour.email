2b2a9fa145118b0a58b772e1600893204aa08717
Andre Arko <andre@arko.net>
2024-09-09 13:01:23 +0700

    add custom matchers so I can tell what's happening

--- spec/requests/inbound_emails_spec.rb
+++ spec/requests/inbound_emails_spec.rb
@@ -1,5 +1,30 @@
 require "rails_helper"
 
+RSpec::Matchers.matcher :accept_email do
+  match do |actual|
+    expect { actual.call }.to change { ActionMailbox::InboundEmail.count }.by(1)
+    expect(response).to be_successful
+  end
+  supports_block_expectations
+end
+
+RSpec::Matchers.matcher :reject_email do
+  match do |actual|
+    expect { actual.call }.to change { ActionMailbox::InboundEmail.count }.by(0)
+    expect(response).to be_forbidden
+  end
+  supports_block_expectations
+end
+
+RSpec::Matchers.matcher :create_post do
+  match do |actual|
+    expect { actual.call }.to change { ActionMailbox::InboundEmail.last }
+    expect { ActionMailbox::InboundEmail.last.route }.to change { Post.count }.by(1)
+  end
+  supports_block_expectations
+end
+RSpec::Matchers.define_negated_matcher :reject_post, :create_post
+
 RSpec.describe "/rails/action_mailbox/postmark/inbound_emails", type: :request do
   let!(:feed) { Feed.create!(token: "v01sntumrlbl20r0yrl6vcsj") }
 
@@ -21,17 +46,14 @@ RSpec.describe "/rails/action_mailbox/postmark/inbound_emails", type: :request d
 
   describe "POST one email" do
     it "saves the inbound email" do
-      expect { post_email }.to change { ActionMailbox::InboundEmail.count }.by(1)
-      expect(response).to be_successful
-      expect { ActionMailbox::InboundEmail.last.route }.to change { feed.posts.count }.by(1)
+      expect { post_email }.to accept_email.and create_post
     end
 
     context "when feed is expired" do
       it "rejects the email" do
         feed.update!(fetched_at: 4.months.ago)
 
-        expect { post_email }.to change { ActionMailbox::InboundEmail.count }.by(0)
-        expect(response).to be_forbidden
+        expect { post_email }.to reject_email
       end
     end
   end
@@ -40,24 +62,16 @@ RSpec.describe "/rails/action_mailbox/postmark/inbound_emails", type: :request d
     context "on day one" do
       it "accepts all emails without warnings" do
         # Regular incoming email
-        expect { post_email("llvm-01") }.to change { ActionMailbox::InboundEmail.count }.by(1)
-        expect(response).to be_successful
-        expect { ActionMailbox::InboundEmail.last.route }.to change { feed.posts.count }.by(1)
+        expect { post_email("llvm-01") }.to accept_email.and create_post
 
         # Would generate a usage warning (but doesn't on day 1)
-        expect { post_email("llvm-02") }.to change { ActionMailbox::InboundEmail.count }.by(1)
-        expect(response).to be_successful
-        expect { ActionMailbox::InboundEmail.last.route }.to change { feed.posts.count }.by(1)
+        expect { post_email("llvm-02") }.to accept_email.and create_post
 
         # Would be weekly limit (but isn't on day 1)
-        expect { post_email("llvm-03") }.to change { ActionMailbox::InboundEmail.count }.by(1)
-        expect(response).to be_successful
-        expect { ActionMailbox::InboundEmail.last.route }.to change { feed.posts.count }.by(1)
+        expect { post_email("llvm-03") }.to accept_email.and create_post
 
         # Past weekly limit (but isn't on day 1)
-        expect { post_email("llvm-04") }.to change { ActionMailbox::InboundEmail.count }.by(1)
-        expect(response).to be_successful
-        expect { ActionMailbox::InboundEmail.last.route }.to change { feed.posts.count }.by(1)
+        expect { post_email("llvm-04") }.to accept_email.and create_post
       end
     end
   end
