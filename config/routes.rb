72e71c53cf34e8b15d6327094e548c273bab7d91
Andre Arko <andre@arko.net>
2024-10-03 17:41:58 +0900

    add a search route and form to posts index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :posts, only: [:show]
   resources :feeds, only: [:new, :create, :show] do
-    resources :posts, only: [:index]
+    resources :posts, only: [:index] do
+      collection { get :search }
+    end
   end
   resources :stats, only: [:index]
 
