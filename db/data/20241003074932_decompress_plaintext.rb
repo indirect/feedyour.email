9f60e08779a42ba84d702701e51d7ec3c502f3de
Andre Arko <andre@arko.net>
2024-10-05 10:48:22 +0900

    better decompression migration
    1. don't sleep between records, if we give the VM one core per ruby process everything is fine
    2. don't iterate over records that have already been decompressed, so we can run repeatedly and eventually complete
    3. why is this still getting OOM killed??? I thought find_each allowed GC to run and then it wouldn't die :(

--- db/data/20241003074932_decompress_plaintext.rb
+++ db/data/20241003074932_decompress_plaintext.rb
@@ -12,20 +12,20 @@ class DecompressPlaintext < ActiveRecord::Migration[7.2]
   using Progress
 
   def up
-    Post.find_each.with_progress do |post|
+    Post.where(text_body: nil).where.not(compressed_text_body: nil)
+      .find_each.with_progress do |post|
       post[:text_body] ||= post[:compressed_text_body]
       post[:compressed_text_body] = nil if post[:text_body]
       post.save!
-      sleep 0.5
     end
   end
 
   def down
-    Post.find_each.with_progress do |post|
+    Post.where(compressed_text_body: nil).where.not(text_body: nil)
+      .find_each.with_progress do |post|
       post[:compressed_text_body] ||= post[:text_body]
       post[:text_body] = nil if post[:compressed_text_body]
       post.save!
-      sleep 0.5
     end
   end
 end
