cf712e86ce5d5da5a7198ab28e07d29fbf44f3ee
Andre Arko <andre@arko.net>
2024-10-05 19:13:03 +0900

    shop shelling out for backup
    also gzip inside github actions because the server only has one tiny cpu

--- app/lib/database_backup.rb
+++ app/lib/database_backup.rb
@@ -4,19 +4,18 @@ class DatabaseBackup
   end
 
   def run
-    `sqlite3 #{Shellwords.escape db_path} "VACUUM INTO '#{Shellwords.escape backup_path}'"`
-    `gzip -f9 #{Shellwords.escape backup_path}`
-
-    "#{backup_path}.gzip"
+    FileUtils.rm_rf backup_path
+    conn.execute "VACUUM INTO #{conn.quote(backup_path.to_s)}"
+    backup_path
   end
 
   private
 
-  def db_path
-    Rails.root.join "storage", Rails.env, "data.sqlite3" # rubocop:disable Lint/Env
+  def conn
+    ActiveRecord::Base.connection
   end
 
   def backup_path
-    Rails.root.join "storage", Rails.env, "backup.sqlite3" # rubocop:disable Lint/Env
+    Rails.root.join("storage", Rails.env, "backup.sqlite3") # rubocop:disable Lint/Env
   end
 end
